5d5e19a4c51fd98bd77fe4f1a226266f7e593fce
Rafael William <rafael.fim@hotmail.com>
2022-01-04 20:06:06 -0300

    share gifs

--- lib/interface/gif_page.dart
+++ lib/interface/gif_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:share/share.dart';
 
 class GifPage extends StatelessWidget {
   final Map _gifData;
@@ -12,6 +13,14 @@ class GifPage extends StatelessWidget {
       appBar: AppBar(
         title: Text(_gifData['title']),
         backgroundColor: Colors.black,
+        actions: [
+          IconButton(
+            onPressed: () {
+              Share.share(_gifData['images']['fixed_height']['url']);
+            },
+            icon: const Icon(Icons.share),
+          )
+        ],
       ),
       backgroundColor: Colors.black,
       body: Center(
